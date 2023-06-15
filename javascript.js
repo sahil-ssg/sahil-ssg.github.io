67647ef58539f933852de6f9c2d4be855f0e1077
sahil gouni <sahil.gouni@bonamisoftware.com>
2023-06-15 16:31:31 +0530

    Initial Commit

--- javascript.js
+++ javascript.js
@@ -1,21 +1,18 @@
-function appendValue(value) {
-    const display = document.getElementById("display");
-    const display_content = display.value;
-    display.value = display_content == '0' ? value : display_content + value;
-}
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    const currentValue = display.value;
+    const newValue = currentValue === '0' ? value : currentValue + value;
+    display.value = newValue;
+  }
 
-function clearValue() {
-    const display = document.getElementById("display");
-    const display_content = display.value
-    display.value = display_content == '0' ? '0' : display_content.slice(0, -1)
-}
-
-function reset() {
-    const display = document.getElementById("display")
-    display.value = '0'
-}
-
-function calculate() {
-    const display = document.getElementById("display")
-    display.value = eval(display.value)
-}
+  function calculate() {
+    const display = document.getElementById('display');
+    const expression = display.value;
+    let result;
+    try {
+      result = eval(expression);
+    } catch (error) {
+      result = 'Error';
+    }
+    display.value = result;
+  }
