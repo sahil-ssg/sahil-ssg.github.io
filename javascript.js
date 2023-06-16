4a06032910f9d32d1360b7e0f95b1b04e58e0bed
sahil gouni <sahil.gouni@bonamisoftware.com>
2023-06-16 09:31:30 +0530

    edits

--- javascript.js
+++ javascript.js
@@ -1,18 +1,28 @@
-function appendToDisplay(value) {
-    const display = document.getElementById('display');
-    const currentValue = display.value;
-    const newValue = currentValue === '0' ? value : currentValue + value;
-    display.value = newValue;
-  }
-
-  function calculate() {
+function calculate() {
     const display = document.getElementById('display');
     const expression = display.value;
     let result;
     try {
-      result = eval(expression);
+        result = eval(expression);
     } catch (error) {
-      result = 'Error';
+        result = 'Error';
     }
     display.value = result;
-  }
+}
+
+function appendValue(value) {
+    const display = document.getElementById("display");
+    const display_content = display.value;
+    display.value = display_content == '0' ? value : display_content + value;
+}
+
+function clearValue() {
+    const display = document.getElementById("display");
+    const display_content = display.value
+    display.value = display_content == '0' ? '0' : display_content.slice(0, -1)
+}
+
+function reset() {
+    const display = document.getElementById("display")
+    display.value = '0'
+}
